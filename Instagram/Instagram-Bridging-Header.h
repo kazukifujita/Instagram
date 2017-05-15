154bb0d24b09db5c1de0caf68969bc2c1e7359b5
藤田 和樹 <fujitakazuki@imac.local>
2017-05-15 16:01:14 +0900

    コミット

---
+++ Instagram/Instagram-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Instagram-Bridging-Header.h
+//  Instagram
+//
+//  Created by 藤田 和樹 on 2017/05/06.
+//  Copyright © 2017年 藤田 和樹. All rights reserved.
+//
+
+#ifndef Instagram_Bridging_Header_h
+#define Instagram_Bridging_Header_h
+
+#import <AdobeCreativeSDKCore/AdobeCreativeSDKCore.h>
+#import <AdobeCreativeSDKImage/AdobeCreativeSDKImage.h>
+
+
+#endif /* Instagram_Bridging_Header_h */
